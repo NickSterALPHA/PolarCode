f2dddbb180667155ce0898cc4e2f0efd24568a5e
NickSterALPHA <elektrokolja@mail.ru>
2023-01-28 01:46:43 +0300

    Change problem with requirements

--- requirements.sh
+++ requirements.sh
@@ -4,7 +4,8 @@ sudo apt-get update
 echo Install requirements 
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9
+sudo apt install python3.10
+sudo apt-get install python3-dev
 sudo apt install python3-pip
 pip install numpy
 pip install matplotlib
