3b66f104d01ea7d9f898c1af491bf22e82b812de
NickSterALPHA <elektrokolja@mail.ru>
2023-01-28 15:55:24 +0300

    Change Req

--- requirements.sh
+++ requirements.sh
@@ -2,13 +2,9 @@
 echo Updating packages 
 sudo apt-get update 
 echo Install requirements 
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10
-sudo apt-get install python3-dev
-sudo apt install python3-pip
-pip install numpy
-pip install matplotlib
+sudo apt-get install python3.8-dev
+sudo apt-get install python3.8-numpy
+sudo apt-get install python3-matplotlib
 sudo apt install libfltk1.3-dev
 
 
