d6a0465c82c6598b4e0bd9e03acee9b936a7a7db
NickSterALPHA <elektrokolja@mail.ru>
2023-01-29 01:35:51 +0300

    Change Requirements and cmake

--- requirements.sh
+++ requirements.sh
@@ -3,8 +3,8 @@ echo Updating packages
 sudo apt-get update 
 echo Install requirements 
 sudo apt-get install python3.8-dev
-sudo apt-get install python3.8-numpy
+sudo apt-get install python3-numpy
 sudo apt-get install python3-matplotlib
-sudo apt install libfltk1.3-dev
+sudo apt-get install libfltk1.3-dev
 
 
